027ec63a0aa8e562cd748d3ef4896962a96f0c22
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-06-22 23:04:46 +0100

    home page to game
    on click on the start the battle will take you to the battle

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 console.log("hello")
+
+const mainDiv = document.getElementById("main")
+ mainDiv.innerHTML = `<section class="home-section">
+ <h1>
+     <span>Rock✊</span>
+     <span>Paper🤚</span>
+     <span>Scissor✌️</span>
+ </h1>
+ <button class="start-btn" onclick='startGame()'>Start the battle</button>
+</section>`
+
+function startGame (e){
+
+    mainDiv.innerHTML = `
+    <section>
+        <button class="game-action">✊</button>
+        <button class="game-action">🤚</button>
+        <button class="game-action">✌️</button>
+    </section>
+    `
+}
