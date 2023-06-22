43cedeef25b1f6349331f238719f2f951cd4d4f3
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-06-22 23:13:11 +0100

    play with human and play with computer

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 console.log("hello")
 
 const mainDiv = document.getElementById("main")
- mainDiv.innerHTML = `<section class="home-section">
+ mainDiv.innerHTML = `<section class=" myBackground home-section">
  <h1>
      <span>Rock✊</span>
      <span>Paper🤚</span>
      <span>Scissor✌️</span>
  </h1>
- <button class="start-btn" onclick='startGame()'>Start the battle</button>
+ <button class="start-btn" onclick='startGame()' id="battle-comp">Start the battle 🤖</button>
+ <button class="start-btn" onclick='startGame()' id="battle-friend">Start the battle 👨</button>
 </section>`
 
 function startGame (e){
 
     mainDiv.innerHTML = `
-    <section>
+    <section class="myBackground centerObj">
+    <div>
         <button class="game-action">✊</button>
         <button class="game-action">🤚</button>
         <button class="game-action">✌️</button>
+    </div>
     </section>
     `
 }
