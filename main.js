60ddea83eb655d1121b337f5d443559689cc3218
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-06-23 00:05:52 +0100

    function added to handle the action key button pressed

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 console.log("hello")
 
 const mainDiv = document.getElementById("main")
- mainDiv.innerHTML = `<section class=" myBackground home-section">
- <h1>
-     <span>Rock✊</span>
-     <span>Paper🤚</span>
-     <span>Scissor✌️</span>
- </h1>
- <button class="start-btn" onclick='startGame()' id="battle-comp">Start the battle 🤖</button>
- <button class="start-btn" onclick='startGame()' id="battle-friend">Start the battle 👨</button>
-</section>`
+ mainDiv.innerHTML = startHtml()
 
-function startGame (e){
 
+function gameHtml (e){
     mainDiv.innerHTML = `
     <section class="myBackground centerObj">
     <div>
-        <button class="game-action">✊</button>
-        <button class="game-action">🤚</button>
-        <button class="game-action">✌️</button>
+        <button class="game-action" onclick='playGame("rock")'>✊</button>
+        <button class="game-action" onclick='playGame("paper")'>🤚</button>
+        <button class="game-action" onclick='playGame("scissors")'>✌️</button>
     </div>
     </section>
     `
+}
+function playGame (element){
+    mainDiv.innerHTML = `<div>${element}</div>`
+}
+function startHtml(){
+    return `<section class=" myBackground home-section">
+    <h1>
+        <span>Rock✊</span>
+        <span>Paper🤚</span>
+        <span>Scissor✌️</span>
+    </h1>
+    <button class="start-btn" onclick='gameHtml()' id="battle-comp">Start the battle 🤖</button>
+    <button class="start-btn" onclick='gameHtml()' id="battle-friend">Start the battle 👨</button>
+   </section>`
 }
