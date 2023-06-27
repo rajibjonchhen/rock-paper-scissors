47331f4d9b096afa1d44da620ed297485e50430d
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-06-27 16:21:05 +0100

    destructure images

---
+++ images.js
@@ -0,0 +1,8 @@
+export default function myImages ()  {
+    return {
+        defaultImg : "./assets/defaultImg.png",
+        scissors : "./assets/scissors.png",
+        paper : "./assets/paper.png",
+        rock : "./assets/rock.png",
+    };
+};
