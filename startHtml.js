d855e12a0a3d0d4e64c7514d8c2015edc39ca5f7
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-06-28 22:47:09 +0100

    destructure startHtml

---
+++ startHtml.js
@@ -0,0 +1,15 @@
+export default function startHtml(){
+    return `<section class=" myBackground home-section">
+    <h1>
+        <span>Rock✊</span>
+        <span>Paper🤚</span>
+        <span>Scissor✌️</span>
+    </h1>
+    <div class="mb-auto">
+        <button class="start-btn"  id="battle-comp">Start the battle 🤖</button>
+        <button class="start-btn" onclick='gameHtml()' id="battle-friend">Start the battle 👨</button>
+   </div>
+    </section>`
+}
+
+
