6f4240b90e133601d900124a7161ee830e7815e5
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-06-30 23:56:23 +0100

    readjusted the section

--- gameHtml.js
+++ gameHtml.js
@@ -4,38 +4,38 @@ import images from "./images.js"
 
 
 export default function gameHtml (){
-    const selectMatch = TotalMatchHtml()
     return  `
-    <section class="myBackground d-flex m-auto justify-content-around">
-    
-    <div>
-        <h1 class="my-text">Player 👨</h1>
-        <div class="img-container" id="selected-img">
-            <img class="rock-paper-scissors-img" src="${images['defaultImg']}" alt="${images['defaultImg']}" />
+    <section>
+        <div class="d-flex m-auto justify-content-around">
+            <div>
+                <h1>Player 👨</h1>
+                <div class="img-container" id="selected-img">
+                    <img class="rock-paper-scissors-img" src="${images['defaultImg']}" alt="${images['defaultImg']}" />
+                </div>
+                <div class="mb-4">
+                    <button class="game-action" id = 'stone-btn' >✊</button>
+                    <button class="game-action" id = 'paper-btn' >🤚</button>
+                    <button class="game-action" id = 'scissors-btn' >✌️</button>
+                </div>
+            </div>
+            <div class="centerObj">
+                <h1 id="vs-text">VS</h1>
+            </div>
+            <div>
+                <h1 class="my-text">Computer 🤖</h1>
+                <div class="img-container" id="com-selected-img">
+                    <img class="rock-paper-scissors-img" src="${images['defaultImg']}" alt="${images['defaultImg']}" />
+                </div>
+                <div>
+                    <button class="game-action" disabled>✊</button>
+                    <button class="game-action" disabled>🤚</button>
+                    <button class="game-action" disabled>✌️</button>
+                </div>
+            </div>
+            <div class='decision-text'>
+                <h2 id="decision" >Click a button of choice to start game and see the result will be displayed here</h2>
+            </div>
         </div>
-        <div class="mb-4">
-            <button class="game-action" id = 'stone-btn' >✊</button>
-            <button class="game-action" id = 'paper-btn' >🤚</button>
-            <button class="game-action" id = 'scissors-btn' >✌️</button>
-        </div>
-    </div>
-    <div>
-        <h1 id="vs-text">VS</h1>
-    </div>
-    <div>
-        <h1 class="my-text">Computer 🤖</h1>
-        <div class="img-container" id="com-selected-img">
-            <img class="rock-paper-scissors-img" src="${images['defaultImg']}" alt="${images['defaultImg']}" />
-        </div>
-        <div>
-            <button class="game-action" disabled>✊</button>
-            <button class="game-action" disabled>🤚</button>
-            <button class="game-action" disabled>✌️</button>
-        </div>
-    </div>
     </section>
-    <div>
-    <h1 id="decision" class='my-text decision-text'></h1>
-</div>
     `
 }
