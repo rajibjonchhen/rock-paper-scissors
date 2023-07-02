e3c6e918c89e2848f5c466c1c5f2aa3c6e2af6db
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-07-02 10:54:59 +0100

    match count updated

--- main.js
+++ main.js
@@ -16,11 +16,17 @@ function routeToGamePage (){
     stoneBtn.addEventListener('click',()=>playerChoice("rock"))
 
     const paperBtn =  document.getElementById('paper-btn')
-    paperBtn.addEventListener('click',()=>playerChoice("paper"))    
+    paperBtn.addEventListener('click',()=>playerChoice("paper"))   
+    
+    const matchCount =  document.getElementById('match-count')
+    matchCount.innerText = localStorage.getItem('playCount')
+    
+    const totalMatch =  document.getElementById('total-match')
+    totalMatch.innerText = localStorage.getItem('bestOf')
 }
 
 const numbMatches = document.getElementsByTagName('select')[0]
-numbMatches.addEventListener('change', (e)=> localStorage.setItem('bestOf',e.target.value))
+numbMatches.addEventListener('change', (e)=> {localStorage.setItem('bestOf',e.target.value); localStorage.setItem('playCount','0') })
 
  const battleComp =  document.getElementById('battle-comp')
  const battlefriend =  document.getElementById('battle-friend')
