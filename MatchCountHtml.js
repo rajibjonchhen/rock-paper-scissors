0438932121721dd90f7c3c58e0282ea0680e5d0a
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-07-02 09:46:21 +0100

    match count added

---
+++ MatchCountHtml.js
@@ -0,0 +1,9 @@
+
+
+export default function MatchCountHtml() {
+  return  `<p class="match-count'>
+        <span id='match-count'></span> /
+        <span id='total-match'></span>
+    </p>`
+  
+}
